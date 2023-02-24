55ab6320e205b1539e477e63b879a2ee7088b7f3
4noidea <110051832+4noidea@users.noreply.github.com>
2023-02-24 12:01:16 +0100

    Add files via upload

---
+++ 24.02.2023.py
@@ -0,0 +1,38 @@
+pierwsza_linijka="*******************";
+druga_linijka="*                 *";
+gwiazdka="*";
+print("zad.1")
+print(pierwsza_linijka);
+print(pierwsza_linijka);
+print(pierwsza_linijka);
+print(pierwsza_linijka);
+print("zad.2")
+print(pierwsza_linijka);
+print(druga_linijka);
+print(druga_linijka);
+print(pierwsza_linijka);
+print("zad.3");
+print(gwiazdka);
+print(gwiazdka,gwiazdka);
+print(gwiazdka,gwiazdka,gwiazdka);
+print(gwiazdka,gwiazdka,gwiazdka,gwiazdka);
+print("zad.4");
+y=(512-282)/((47*48)+5);
+print(y);
+print("zad.5");
+x=7;
+print(x,2*x,3*x,4*x,5*x,sep="---");
+print("zad.6");
+tekst="Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. " \
+      "Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki." \
+      " Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym." \
+      " Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+print(tekst);
+print("zad.7");
+a=eval(input('wpisz a:'));
+b=eval(input('wpisz b:'));
+print(a*b);
+print("zad.8");
+waga=eval(input('podaj wage w kg:'))
+
+print("podana waga w funtach to:", waga*2.2)
